b55a6a448e37ea78245fe8d4f8a89f63993075d8
Timur <zhilin.timur06@mail.ru>
2023-11-30 10:46:25 +0300

    Добавьте файлы проекта.

---
+++ pr 5 z3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr_5_z3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (double.TryParse(priceTextBox.Text, out double pricePerKg))
+            {
+
+                resultTextBlock.Text = "";
+
+
+                for (int kg = 1; kg <= 10; kg++)
+                {
+                    double totalCost = pricePerKg * kg;
+                    resultTextBlock.Text += $"{kg} кг: {totalCost:C}\n";
+                }
+            }
+        }
+    }
+}
+
+
+
+       
